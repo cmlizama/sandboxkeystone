9f87f35203567b98a0d4b06a58868feeec091512
gemfarmer <bshslowfood@gmail.com>
2014-07-10 17:55:58 -0500

    changes at the walker

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,14 +2,14 @@ var keystone = require('keystone'),
 	_ = require('underscore');
 
 exports = module.exports = function(req, res) {
-	
+
 	var locals = res.locals,
 		view = new keystone.View(req, res);
-	
+
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+
   //load the members
   var Members = keystone.list('Members');
   // var view, etc. as in the existing screens
@@ -19,12 +19,12 @@ exports = module.exports = function(req, res) {
       	members[i].index = i+1;
       };
       locals.members = members;
-      
+
       next();
     });
   });
 
 	// Render the view
 	view.render('index');
-	
+
 };
