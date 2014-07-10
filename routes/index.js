4d9801b8e7d552d1cfbb394167475f1f021359de
Chris Lizama <christopher.lizama@gmail.com>
2014-07-09 20:53:49 -0500

    fixed a bug with 'members' drop down
    the reactively defined ‘members’ tab broke across most of the site
    because the ‘members’ object was not globally available. this bug fix
    makes it so that the members object is available everywhere

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ var _ = require('underscore'),
 // Common Middleware
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
+keystone.pre('routes', middleware.loadMembers);
 
 // Import Route Controllers
 var routes = {
