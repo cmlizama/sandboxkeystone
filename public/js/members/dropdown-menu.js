4ca86bd34077e4ce53ae522d2c8f2e78343fc482
gemfarmer <bshslowfood@gmail.com>
2014-07-16 21:31:31 -0500

    changes to dropdown

--- public/js/members/dropdown-menu.js
+++ public/js/members/dropdown-menu.js
@@ -1,3 +1,23 @@
 $(function() {
+	$('.show-dropdown.member-link').on('click',function(){
+			var windowHeight = $(window).height();
+			var headerHeight = $('.navbar').height();
+			var collapseFlag = $('.navbar-collapse').hasClass('collapse');
 
+			if($('#header').hasClass('dropdown')){
+				$('#header').toggleClass('dropdown');
+				$('#header').animate({height:headerHeight}, 500);
+				$('.bullet-nav').css({'display':'none'});
+			} else {
+				$('#header').toggleClass('dropdown');
+				$('#header').animate({height:windowHeight}, 500);
+				$('.bullet-nav').css({'display':'block'});
+			}
+
+
+
+			// }
+
+
+	});
 });
