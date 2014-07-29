18a1e991a16df84485e84e6855c749ab1298b936
Chris Lizama <christopher.lizama@gmail.com>
2014-07-29 16:27:42 -0500

    font, styling and footer changes

---
+++ models/siteAssets.js
@@ -0,0 +1,13 @@
+var keystone = require('keystone'),
+  Types = keystone.Field.Types;
+
+
+var SiteAssets = new keystone.List('SiteAssets', {
+  autokey: { from: 'name', path: 'key', unique: true}
+});
+
+SiteAssets.add({
+  nextArrow: { type: Types.CloudinaryImage}
+});
+
+SiteAssets.register();
