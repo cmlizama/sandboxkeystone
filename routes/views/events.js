17bd44ebe37bdb025f72c7c37decebdb405d5cd5
Chris Lizama <christopher.lizama@gmail.com>
2014-08-10 17:00:54 -0500

    fixed errors with the footer

--- routes/views/events.js
+++ routes/views/events.js
@@ -6,6 +6,16 @@ exports = module.exports = function(req, res) {
     view = new keystone.View(req, res);
 
     //locals.section = 'events';
+
+  //load the members
+  var Members = keystone.list('Members');
+  // var view, etc. as in the existing screens
+  view.on('init', function(next) {
+      Members.model.find().sort('name').exec(function(err, members) {
+        locals.members = members;
+        next();
+      });
+  });
     
   view.render('events');
 };
