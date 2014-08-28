321b6f31b448afcd9ee85e89960f09e7da3c8570
Chris Lizama <christopher.lizama@gmail.com>
2014-08-27 20:02:01 -0500

    'connect' view and needed data

--- models/Connect.js
+++ models/Connect.js
@@ -1,1 +1,15 @@
+var keystone = require('keystone'),
+  Types = keystone.Field.Types;
 
+var ConnectInfo = new keystone.List('ConnectInfo');
+
+ConnectInfo.add({
+  name: {type: String, required: true},
+  newsletterLink: {type: Types.Url},
+  connectFormLink: {type: Types.Url},
+  pitchLead: {type: String},
+  pitch: {type: Types.Textarea},
+  connectImage: {type: Types.CloudinaryImage}
+});
+
+ConnectInfo.register();
