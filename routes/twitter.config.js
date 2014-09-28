9f3660a925ac24adbd9c1233fd8d1a196af1f8e6
gemfarmer <bshslowfood@gmail.com>
2014-09-28 17:11:53 -0500

    lazy load

---
+++ routes/twitter.config.js
@@ -0,0 +1,8 @@
+module.exports = function(req, res) {
+	return {
+		"API_key":"yrwzjlccPkkVmiUTafGjyrFEb",
+		"API_secret": "nivsrR0wzjFk5GKftRaYOCl3aTU0ron3Gvxq1uKYjIzw7F0B0Y",
+		"Access_token":	"244970777-3AA0oGylhBwfFYAygsfAQPReiSY4noFBxQSFhMvo",
+		"Access_token_secret":"o5dL5IETowjQcpOuVzqsrH2gxUCP8pMvqRamWLe0NdvCx"
+	}
+}
--- routes/views/index.js
+++ routes/views/index.js
@@ -15,22 +15,26 @@ exports = module.exports = function(req, res) {
   //console.log(Members)
   // var view, etc. as in the existing screens
   view.on('init', function(next) {
-    Members.model.find().sort('name').exec(function(err, members) {
-      //console.log(members)
-      for (var i = members.length - 1; i >= 0; i--) {
-      	members[i].index = i+1;
-        var nextIndex = i+1;
-        if (nextIndex == members.length){
-          members[i].nextId = members[i]._id;
-        } else {
-          members[i].nextId = members[nextIndex]._id;
-        }
+    setTimeout(function(){
 
-      };
-      locals.members = members;
+      Members.model.find().sort('name').exec(function(err, members) {
+        //console.log(members)
+        for (var i = members.length - 1; i >= 0; i--) {
+        	members[i].index = i+1;
+          var nextIndex = i+1;
+          if (nextIndex == members.length){
+            members[i].nextId = members[i]._id;
+          } else {
+            members[i].nextId = members[nextIndex]._id;
+          }
 
-      next();
-    });
+        };
+        locals.members = members;
+
+        next();
+      });
+
+    },1000);
   });
   //console.log('index js file');
 
